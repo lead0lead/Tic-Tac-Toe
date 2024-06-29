fe19496670b4de576125db7d831ff372d8fb267c
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-06-29 21:19:51 +0200

    Add player, grid and cell classes
    create files for player, grid and cell class
    add variables to cell and grid class

---
+++ lib/grid.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# The board of the game, made from cells
+class GRID
+  def initialize(grid_column_amount, grid_row_amount)
+    @grid_column_amount = grid_column_amount
+    @grid_row_amount = grid_row_amount
+  end
+
+  def create_grid(grid_column_amount, grid_row_amount)
+
+  end
+end
