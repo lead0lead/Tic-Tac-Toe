c38c264cc4f2ebbf5169300d35dcac5b07593206
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-08 16:14:37 +0200

    Restructure project, add tutorial

---
+++ lib/game.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class GAME
+  @grid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  def print_grid
+    p @grid
+  end
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-require_relative 'lib/cell'
-require_relative 'lib/player'
-require_relative 'lib/grid'
+require_relative 'lib/game'
+
+yee = GAME.new
+
+puts 'Welcome to Tic Tac Toe. Each turn, enter two coordinates, separated by a comma [,] as shown in the grid below:'
+puts "\r\n"
+puts '0,0 | 0,1 | 0,2'
+puts '1,0 | 1,1 | 1,2'
+puts '2,0 | 2,1 | 0,2'
